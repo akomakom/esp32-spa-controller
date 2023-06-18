1c62efb66bfca39cb37cffa2a7084ad49a9d9143
Akom <regs@akom.net>
2023-06-17 20:26:36 -0400

    Code style - default CLion

--- HotTubUtils.h
+++ HotTubUtils.h
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
@@ -18,61 +19,70 @@ const int RELAY_PIN_8 = 13;
 const int LED_PIN = 23;
 
 class SpaControl {
-  const int DEFAULT_MIN = 0;
-  const int DEFAULT_MAX = 1;
-  public:
-    SpaControl(const char* name);
-    SpaControl(const char* name, u_int8_t min, u_int8_t max);
+    const int DEFAULT_MIN = 0;
+    const int DEFAULT_MAX = 1;
+public:
+    SpaControl(const char *name);
+
+    SpaControl(const char *name, u_int8_t min, u_int8_t max);
+
     void toggle();
+
     void applyOutputs();
 
-    const char* name;
+    const char *name;
     u_int8_t min = DEFAULT_MIN;
     u_int8_t max = DEFAULT_MAX;
     u_int8_t value = 0;
 
-  private:
-    void init(const char* name, u_int8_t min, u_int8_t max);
+private:
+    void init(const char *name, u_int8_t min, u_int8_t max);
 };
 
 class SimpleSpaControl : public SpaControl {
-  public:
-    SimpleSpaControl(const char* name, u_int8_t pin);
-    SimpleSpaControl(const char* name, u_int8_t pin, u_int8_t min, u_int8_t max);
+public:
+    SimpleSpaControl(const char *name, u_int8_t pin);
+
+    SimpleSpaControl(const char *name, u_int8_t pin, u_int8_t min, u_int8_t max);
+
     void toggle();
+
     void applyOutputs();
+
     u_int8_t pin;
 };
-
-// class TwoSpeedSpaControl : public SpaControl {
-//   public:
-//     TwoSpeedSpaControl(const char* name, u_int8_t pin_power, u_int8_t pin_speed) : SpaControl(name);
-// };
+//
+//class TwoSpeedSpaControl : public SpaControl {
+//public:
+//    TwoSpeedSpaControl(const char *name, u_int8_t pin_power, u_int8_t pin_speed) : SpaControl(name);
+//};
 
 class SpaStatus {
-  private:
+private:
     StaticJsonDocument<200> jsonStatus;
     JsonObject jsonStatusControls = jsonStatus.createNestedObject("controls");
     JsonObject jsonStatusMetrics = jsonStatus.createNestedObject("metrics");
 
-  public:
+public:
     SpaStatus();
-    SpaControl* pump      = new SimpleSpaControl("pump",      RELAY_PIN_1); 
-    SpaControl* pump_fast = new SimpleSpaControl("pump_fast", RELAY_PIN_2); 
-    SpaControl* blower    = new SimpleSpaControl("blower",    RELAY_PIN_3);
-    SpaControl* heater    = new SimpleSpaControl("heater",    RELAY_PIN_4);
-    SpaControl* ozone     = new SimpleSpaControl("ozone",     RELAY_PIN_5);
+
+    SpaControl *pump = new SimpleSpaControl("pump", RELAY_PIN_1);
+    SpaControl *pump_fast = new SimpleSpaControl("pump_fast", RELAY_PIN_2);
+    SpaControl *blower = new SimpleSpaControl("blower", RELAY_PIN_3);
+    SpaControl *heater = new SimpleSpaControl("heater", RELAY_PIN_4);
+    SpaControl *ozone = new SimpleSpaControl("ozone", RELAY_PIN_5);
 
 
     char statusString[100];
+
     void updateStatusString();
 
-    SpaControl* controls[5] = { pump, pump_fast, blower, heater, ozone };
+    SpaControl *controls[5] = {pump, pump_fast, blower, heater, ozone};
 
-    SpaControl* findByName(const char* name);
+    SpaControl *findByName(const char *name);
 
     void applyControls();
-  
+
 };
 
 
